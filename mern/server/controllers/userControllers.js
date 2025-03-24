d918d257af9fa23b78230299121a8d4f67d727c8
javi <jae20@gcloud.ua.es>
2025-03-24 19:14:41 +0100

    add user con mas parametros

--- mern/server/controllers/userControllers.js
+++ mern/server/controllers/userControllers.js
@@ -13,13 +13,13 @@ export const getUsers = async (req, res) => {
 
 export const addUser = async (req, res) => {
     try {
-        const { nombre, correo, pw } = req.body;
+        const { nombre, correo, pw, apellido, usuario, pais } = req.body;
 
-        if (!nombre || !correo || !pw) {
+        if (!nombre || !correo || !pw || !apellido || !usuario || !pais) {
             return res.status(400).json({ error: "Todos los campos son obligatorios" });
         }
 
-        const newUser = await createUser({ nombre, correo, pw }); // Guardar en la BD
+        const newUser = await createUser({ nombre, correo, pw, apellido, usuario, pais}); // Guardar en la BD
         res.status(201).json({ message: "Usuario creado", id: newUser.insertedId });
     } catch (error) {
         res.status(400).json({ error: "Error al crear usuario" });
