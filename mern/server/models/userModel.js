5346aa896481834a3664b0a14840d7d14ae120e9
javi <jae20@gcloud.ua.es>
2025-04-09 20:17:15 +0200

    Mas backend

--- mern/server/models/userModel.js
+++ mern/server/models/userModel.js
@@ -1,4 +1,5 @@
 //consultas a la bdd
+import { ObjectId } from "mongodb";
 import { client } from "../db/connection.js";
 
 const db = client.db("UA-bdd");
@@ -9,4 +10,22 @@ export const getAllUsers = async () => {
 
 export const createUser = async (userData) => {
     return await db.collection("User").insertOne(userData);
+};
+
+// Obtener usuario por ID
+export const getUserById = async (id) => {
+    return await db.collection("User").findOne({ _id: new ObjectId(id) });
+};
+
+// Actualizar usuario
+export const updateUser = async (id, updatedData) => {
+    return await db.collection("User").updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData }
+    );
+};
+
+// Eliminar usuario
+export const deleteUser = async (id) => {
+    return await db.collection("User").deleteOne({ _id: new ObjectId(id) });
 };
--- mern/server/routes/userRoutes.js
+++ mern/server/routes/userRoutes.js
@@ -1,11 +1,14 @@
 //definicion endpoints
 import express from "express";
-import { addUser, getUsers, loginUser } from "../controllers/userControllers.js";
+import { addUser, borrarUser, editUser, getUser, getUsers, loginUser } from "../controllers/userControllers.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);  // get usuarios
 router.post("/", addUser);  // post usuario
 router.post("/login", loginUser); // inicio sesion
+router.get("/:id", getUser); // Obtener usuario por ID
+router.put("/:id", editUser); // Editar usuario
+router.delete("/:id", borrarUser); // Eliminar usuario
 
 export default router;
